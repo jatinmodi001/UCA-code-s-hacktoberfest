ff20caa9b0c8b890a64de9798f3f87ff8a17a4d9
jatinmodi001 <jatinmodi0001@gmail.com>
2019-08-02 23:02:40 +0530

    Using Bitwise Operators

--- Bitwise/EncodingScheme.c
+++ Bitwise/EncodingScheme.c
@@ -13,28 +13,38 @@ char* encode(char* input)
     int n = strlen(input);
     int size = n*8;
     int ct = 0,j=0;
-    char *output = (char*)calloc((n*4)/3+5,sizeof(char));
+    char *output = (char*)calloc((n*4)/3+5,sizeof(char));			 	
     for(int i=0;i<=n;i++)
     {
-        if(ct == 0 && i!=n)
+        if(ct == 0 && i!=n)				
         {
-            output[j++] = s[(int)input[i] >> 2];
+
+/*	getting character fromed from last 6 bits of ith character ascii value, by right shifting first 2 bits 		*/
+
+            output[j++] = s[(int)input[i] >> 2];		
             ct = 1;
         }
         else if(ct == 1)
-        {
-            output[j++] = s[((int)input[i-1] & 0X03) << 4  | ((int)input[i] & 0XF0) >> 4];
+       	{
+       	
+/*	getting character formed from 2 bits of previous character ascii value and 4 bits from the ith character 	*/
+
+            output[j++] = s[((int)input[i-1] & 0X03) << 4  | ((int)input[i] & 0XF0) >> 4];	// & with 00000011, 11110000 so as to get last 2 and first 4
             ct = 2;
         }
         else if(ct == 2)
         {
-            output[j++] = s[((int)input[i-1] & 0X0F ) << 2 | ((int)input[i] & 0XC0) >> 6 ];
+/*	getting character formed from 4 bits of previous character ascii value and 2 bits of ith character 	*/
+
+            output[j++] = s[((int)input[i-1] & 0X0F ) << 2 | ((int)input[i] & 0XC0) >> 6 ];	// & with 00001111, 11000000 so as to get last 4 and first 2
             ct = 3;
         }
         else if(ct == 3)
         {
-            output[j++] = s[(int)input[i-1] & 0X3F ];
-            i--;
+/*	getting character formed from 6 bits of previous character	*/
+
+            output[j++] = s[(int)input[i-1] & 0X3F ];		//	& with 00111111 so as to get last 6 bits
+            i--;						//	at this stage character input[i] skips if didn't do this (i--).
             ct = 0;
         }
     }
@@ -48,25 +58,14 @@ char* encode(char* input)
     }
     return output;
 }
-int main() {
-    char *output_path = getenv("OUTPUT_PATH");
-    FILE *f;
-    if (output_path != NULL) {
-        f = fopen(output_path, "w");
-    }
-    else {
-        f = stdout;
-    }
-
-    char* res;
-    char* input;
-    input = (char *)malloc(512000 * sizeof(char));
+int main() 
+{
+    char* input = (char *)malloc(512000 * sizeof(char));
     scanf("\n%[^\n]",input);
 
-    res = encode(input);
-    fprintf(f, "%s\n", res);
+    char *res = encode(input);
+    printf("%s\n", res);
 
-    fclose(f);
     return 0;
 }
 
