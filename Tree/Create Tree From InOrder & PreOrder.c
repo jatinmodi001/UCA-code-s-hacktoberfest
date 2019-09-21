deb5af9695a1f4948370a22abd6c911097749f55
jatinmodi001 <jatinmodi0001@gmail.com>
2019-09-21 08:32:25 +0530

    Updated

--- Tree/Create Tree From InOrder & PreOrder.c
+++ Tree/Create Tree From InOrder & PreOrder.c
@@ -15,23 +15,23 @@ int search(int in[],int start,int end,int k)          // Searching given num in 
             return i;
     }
 }
-Tree* createTree(int in[],int post[], int start,int end,int *index)     // creating Tree
+Tree* createTree(int in[],int pre[], int start,int end,int *index)     // creating Tree
 {
     if(start > end)
         return NULL;
     
-    int i = search(in,start,end,post[(*index)--]);      // finding node in inorder array
+    int i = search(in,start,end,pre[(*index)--]);      // finding node in inorder array
     Tree *newnode = (Tree*)malloc(sizeof(Tree));
     newnode->val = in[i];
 
 /*	as in inorder the left part of node will be lie in the left position and
 	right part of node will be lie in right position so....		*/
+     
+    newnode->left = createTree(in,pre,start,i-1,index);
+    newnode->right = createTree(in,pre,i+1,end,index);
 
-    newnode->right = createTree(in,post,i+1,end,index);     
-    newnode->left = createTree(in,post,start,i-1,index);
-
-    /* right is taken first because in post order is left->right->root and
-     we start from end here so after root we have to find right of that node */ 
+    /* left is taken first because in pre order is root->left->right and
+     we start from end here so after root we have to find left of that node */ 
     return newnode;     
 }
 
@@ -40,17 +40,17 @@ int main()
     int n;
     scanf("%d",&n);
     int inorder[n];
-    int postorder[n];
+    int preorder[n];
     for(int i=0;i<n;i++)
     {
         scanf("%d",&inorder[i]);        // scanning in-order  array
     }
     for(int i=0;i<n;i++)
     {
-        scanf("%d",&postorder[i]);      // scanning post-order array
+        scanf("%d",&preorder[i]);      // scanning pre-order array
     }
     n--;
-    Tree *root = createTree(inorder,postorder,0,n,&n); 
+    Tree *root = createTree(inorder,preorder,0,n,&n); 
 
     /* here root will be the main binary tree   */
 
